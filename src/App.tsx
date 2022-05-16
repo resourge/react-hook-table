b7ac5170abab9334b980a1fdf1be0678d5e30761
resourge <jdaoliveiraprograming@hotmail.com>
2022-05-16 14:11:26 +0100

    refactor(app): remove app example

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,8 @@
-import { useState } from 'react'
-
-import { useTable } from './lib'
-
-type Product = {
-	id: number
-	name: string
-}
-
 function App() {
-	const [
-		{
-			products
-		},
-		{
-			handleTable, changeItemsPerPage, reset
-		}
-	] = useTable<{ products: Product[] }>(
-		{
-			products: []
-		}
-	)
-
-	const [random, setRandom] = useState(Math.random);
-
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	const [_, setRandom2] = useState(Math.random);
-
-	const manually = handleTable((params) => {
-		console.log('handleTable params', JSON.stringify(params, null, 4))
-		reset(
-			{ 
-				products: [
-					{ id: Math.random(), name: 'Apple' }
-				] 
-			}, 
-			10
-		);
-	}, [random])
-
-	console.log('random', random)
-
 	return (
-		<>
-			<button onClick={() => setRandom(Math.random())}>
-				Update deps
-			</button>
-			<button onClick={() => setRandom2(Math.random())}>
-				Update deps2
-			</button>
-			<button onClick={manually}>
-				Update manually
-			</button>
-			<button onClick={() => {
-				changeItemsPerPage(100)
-			}}>
-				Update manually
-			</button>
-			<table>
-				<tbody>
-					{
-						products.map((product, index) => (
-							<tr key={`${index}`}>
-								<td>{ product.id } </td>
-								<td>{ product.name } </td>
-							</tr>
-						))
-					}
-				</tbody>
-			</table>
-		</>
+		<div>
+			App
+		</div>
 	)
 }
 
---
+++ src/examples/App.tsx
@@ -0,0 +1,75 @@
+import { useState } from 'react'
+
+import { useTable } from '../lib'
+
+type Product = {
+	id: number
+	name: string
+}
+
+function App() {
+	const [
+		{
+			products
+		},
+		{
+			handleTable, changeItemsPerPage, reset
+		}
+	] = useTable<{ products: Product[] }>(
+		{
+			products: []
+		}
+	)
+
+	const [random, setRandom] = useState(Math.random);
+
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	const [_, setRandom2] = useState(Math.random);
+
+	const manually = handleTable((params) => {
+		console.log('handleTable params', JSON.stringify(params, null, 4))
+		reset(
+			{ 
+				products: [
+					{ id: Math.random(), name: 'Apple' }
+				] 
+			}, 
+			10
+		);
+	}, [random])
+
+	console.log('random', random)
+
+	return (
+		<>
+			<button onClick={() => setRandom(Math.random())}>
+				Update deps
+			</button>
+			<button onClick={() => setRandom2(Math.random())}>
+				Update component 
+			</button>
+			<button onClick={manually}>
+				Update manually
+			</button>
+			<button onClick={() => {
+				changeItemsPerPage(100)
+			}}>
+				Change Items per page to 100
+			</button>
+			<table>
+				<tbody>
+					{
+						products.map((product, index) => (
+							<tr key={`${index}`}>
+								<td>{ product.id } </td>
+								<td>{ product.name } </td>
+							</tr>
+						))
+					}
+				</tbody>
+			</table>
+		</>
+	)
+}
+
+export default App
